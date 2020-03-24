406b6395603d1383c919059236848b1b193b393a
Pawel Borowik <p.potozki@interia.pl>
2020-03-25 00:17:44 +0100

    Add show flag in app component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 
 function App() {
 
-  const [countries, setCountries] = useState(null)
+  const [countries, setCountries] = useState([]);
+  const [selectItem, setSelectItem] = useState("")
 
   const API = "https://restcountries.eu/rest/v2/"
 
@@ -19,11 +20,38 @@ function App() {
       })
       .then(response => response.json())
       .then(data => {
+
         setCountries(data)
       })
       .catch(error => console.log(error))
 
   }
+  const handleChangeSelect = (e) => {
+
+    setSelectItem(e.target.value)
+
+  }
+
+  const showCountries = countries.map(country => <option key={country.name}
+    value={country.name}>{country.name}</option>)
+
+
+
+
+
+
+  const flag = countries.filter(item => item.name === selectItem)
+  const bbbb = flag.map(item => <img src={item.flag} alt={item.name}></img>)
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -32,8 +60,14 @@ function App() {
     <div className="App">
       <header className="App-header">
 
-        ffffddd
-      {handleDataFetch()}
+
+        {handleDataFetch()}
+        <select onChange={handleChangeSelect}>
+          <option value="">Select country:</option>
+          {showCountries}
+        </select>
+        <div className="flag">{bbbb}</div>
+
 
 
 
