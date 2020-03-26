e4e471f4625011834738a4babe8f61c2ef2660ea
Pawel Borowik <p.potozki@interia.pl>
2020-03-26 12:01:42 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,76 +1,57 @@
-import React, { useState } from 'react';
-import './App.css';
-
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-
   const [countries, setCountries] = useState([]);
-  const [selectItem, setSelectItem] = useState("")
+  const [selectItem, setSelectItem] = useState("");
 
-  const API = "https://restcountries.eu/rest/v2/"
+  const API = "https://restcountries.eu/rest/v2/";
 
   const handleDataFetch = () => {
-
     fetch(API)
       .then(response => {
         if (response.ok) {
-          return response
+          return response;
         }
-        throw Error("bbbb")
+        throw Error("bbbb");
       })
       .then(response => response.json())
       .then(data => {
-
-        setCountries(data)
+        setCountries(data);
       })
-      .catch(error => console.log(error))
+      .catch(error => console.log(error));
+  };
+  const handleChangeSelect = e => {
+    setSelectItem(e.target.value);
+  };
 
-  }
-  const handleChangeSelect = (e) => {
+  const showCountries = countries.map(country => (
+    <option key={country.name} value={country.name}>
+      {country.name}
+    </option>
+  ));
 
-    setSelectItem(e.target.value)
-
-  }
-
-  const showCountries = countries.map(country => <option key={country.name}
-    value={country.name}>{country.name}</option>)
-
-
-
-
-
-
-  const flag = countries.filter(item => item.name === selectItem)
-  const bbbb = flag.map(item => <img src={item.flag} alt={item.name}></img>)
-
-
-
-
-
-
-
-
-
-
-
-
-
+  const selectedCountry = countries.filter(
+    country => country.name === selectItem
+  );
+  const flag = selectedCountry.map(selectCountry => (
+    <div key={selectCountry.name}>
+      <img src={selectCountry.flag} alt={selectCountry.alpha3Code}></img>
+      <p>{selectCountry.name}</p>
+      <p>({selectCountry.nativeName})</p>
+      <p>Capital city: {selectCountry.capital}</p>
+    </div>
+  ));
 
   return (
     <div className="App">
       <header className="App-header">
-
-
         {handleDataFetch()}
         <select onChange={handleChangeSelect}>
           <option value="">Select country:</option>
           {showCountries}
         </select>
-        <div className="flag">{bbbb}</div>
-
-
-
-
+        <div className="flag">{flag}</div>
       </header>
     </div>
   );
